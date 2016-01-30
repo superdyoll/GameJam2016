14f183e2c5d71fd83704142cb1cd2ebb97853f86
superdyoll <superdyoll@gmail.com>
2016-01-30 13:15:14 +0000

    The bird flys

--- Assets/CrowBehaviour.cs
+++ Assets/CrowBehaviour.cs
@@ -5,8 +5,9 @@ public class CrowBehaviour : MonoBehaviour {
 
 	private bool selected;
 	public Animator animation;
-	private Transform target;
-	public float speed;
+	private Vector2 target;
+	private float speed;
+	public float levelSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +16,9 @@ public class CrowBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (target != null) {
+		if ((Vector2) transform.position != target) {
 			float step = speed * Time.deltaTime;
-			transform.position = Vector2.MoveTowards (transform.position, target.position, step);
+			transform.position = Vector2.MoveTowards (transform.position, target, step);
 		}
 
 		if (Input.GetMouseButtonDown(0)) {
@@ -37,17 +38,21 @@ public class CrowBehaviour : MonoBehaviour {
 		if (hit) {
 			animation.speed = 0;
 			selected = true;
+			speed =0;
 		} else {
 			animation.speed = 1;
+			speed = levelSpeed;
 			selected = false;
 		}
 	}
 
 	void CastMoveRay(){
-		Debug.Log ("Crow selected now moving");
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
-		target.position = hit.point;
+		target = ray.origin;
+		animation.speed = 1;
+		speed = levelSpeed;
+		Debug.Log ("Crow selected now moving to " + target);
 	}
 
 }
