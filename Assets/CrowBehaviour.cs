aeab6e72fdc5705910d817ebfda5332169db597e
superdyoll <superdyoll@gmail.com>
2016-01-30 10:19:40 +0000

    Crow behaviour script

---
+++ Assets/CrowBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowBehaviour : MonoBehaviour {
+
+	bool selected;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			if ( Input.GetMouseButtonDown(0))
+			{
+				hit = RaycastHit;
+				Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				
+				if (Physics.Raycast (ray, hit, 100.0))
+				{  
+					Destroy(GameObject.Find("targetArea"));
+				}
+			}
+}
+}
